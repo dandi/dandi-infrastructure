6ce81aab0325d91c57169a2dce0e40d49455fb8f
Sandy Hider <sandy.hider@jhuapl.edu>
2025-01-29 16:59:04 -0500

    updated api subdomain name to api-dandi-staging

--- terraform/staging_pipeline.tf
+++ terraform/staging_pipeline.tf
@@ -8,7 +8,7 @@ module "api_staging" {
   project_slug     = "ember-dandi-api-staging"
   heroku_team_name = data.heroku_team.dandi.name
   route53_zone_id  = aws_route53_zone.dandi.zone_id
-  subdomain_name   = "api-staging"
+  subdomain_name   = "api-dandi-staging"
 
   heroku_web_dyno_size    = "eco" // "basic"
   heroku_worker_dyno_size = "eco" // "basic"
@@ -20,8 +20,8 @@ module "api_staging" {
   heroku_worker_dyno_quantity = 1
 
   django_default_from_email          = "bbqs-ember-admin@jhuapl.edu"
-  django_cors_origin_whitelist       = ["https://gui-staging-dandi.ember-archive.org"]
-  django_cors_origin_regex_whitelist = ["^https:\\/\\/[0-9a-z\\-]+--gui-staging-dandi.ember-archive-org\\.netlify\\.app$"]
+  django_cors_origin_whitelist       = ["https://gui-staging-dandi.emberarchive.org"]
+  django_cors_origin_regex_whitelist = ["^https:\\/\\/[0-9a-z\\-]+--gui-staging-dandi.emberarchive-org\\.netlify\\.app$"]
 
   additional_django_vars = {
     DJANGO_CONFIGURATION                           = "HerokuStagingConfiguration"
@@ -38,9 +38,9 @@ module "api_staging" {
     DJANGO_SENTRY_DSN                              = data.sentry_key.this.dsn_public
     DJANGO_SENTRY_ENVIRONMENT                      = "staging"
     DJANGO_CELERY_WORKER_CONCURRENCY               = "2"
-    DJANGO_DANDI_WEB_APP_URL                       = "https://gui-staging-dandi.ember-archive.org"
-    DJANGO_DANDI_API_URL                           = "https://api-staging-dandi.ember-archive.org"
-    DJANGO_DANDI_JUPYTERHUB_URL                    = "https://hub-dandi.ember-archive.org/"
+    DJANGO_DANDI_WEB_APP_URL                       = "https://gui-dandi-staging.emberarchive.org"
+    DJANGO_DANDI_API_URL                           = "https://api-dandi-staging.emberarchive.org"
+    DJANGO_DANDI_JUPYTERHUB_URL                    = "https://hub-dandi.emberarchive.org/"
     DJANGO_DANDI_DEV_EMAIL                         = var.dev_email
   }
   additional_sensitive_django_vars = {
