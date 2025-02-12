04391a34b47aa81d49d286e142a2db6a82d03da5
Sandy Hider <sandy.hider@jhuapl.edu>
2025-02-12 17:52:20 -0500

    changed bucket names from having embargo to private

--- terraform/sponsored_bucket.tf
+++ terraform/sponsored_bucket.tf
@@ -14,10 +14,10 @@ module "sponsored_dandiset_bucket" {
 
 module "sponsored_embargo_bucket" {
   source          = "./modules/dandiset_bucket"
-  bucket_name     = "ember-dandi-archive-embargo"
+  bucket_name     = "ember-dandi-archive-private"
   versioning      = false
   heroku_user     = data.aws_iam_user.api
-  log_bucket_name = "ember-dandi-archive-embargo-logs"
+  log_bucket_name = "ember-dandi-archive-private-logs"
   providers = {
     aws         = aws
     aws.project = aws
