b83b30fbd92287aa7575b642dbfc8f6e1e86c7b8
Nicole Stock <nicole.stock@jhuapl.edu>
2025-02-19 13:11:58 -0500

    Commented our email_forwarding.tf since improvmx_domain.dandiarchive is hanging during Apply

--- terraform/email_forwarding.tf
+++ terraform/email_forwarding.tf
@@ -1,28 +1,28 @@
-resource "improvmx_domain" "dandiarchive" {
-  domain = "emberarchive.org"
-}
-
-# Set up email forwards.
-resource "improvmx_email_forward" "help" {
-  domain            = improvmx_domain.dandiarchive.domain
-  alias_name        = "help"
-  destination_email = "emberarchive@jhuapl.edu"
-}
-
-resource "improvmx_email_forward" "info" {
-  domain            = improvmx_domain.dandiarchive.domain
-  alias_name        = "info"
-  destination_email = "emberarchive@jhuapl.edu"
-}
-
-resource "improvmx_email_forward" "team" {
-  domain            = improvmx_domain.dandiarchive.domain
-  alias_name        = "team"
-  destination_email = "emberarchive@jhuapl.edu"
-}
-
-resource "improvmx_email_forward" "community" {
-  domain            = improvmx_domain.dandiarchive.domain
-  alias_name        = "community"
-  destination_email = "emberarchive@jhuapl.edu"
-}
+// resource "improvmx_domain" "dandiarchive" {
+//   domain = "emberarchive.org"
+// }
+// 
+// # Set up email forwards.
+// resource "improvmx_email_forward" "help" {
+//   domain            = improvmx_domain.dandiarchive.domain
+//   alias_name        = "help"
+//   destination_email = "emberarchive@jhuapl.edu"
+// }
+// 
+// resource "improvmx_email_forward" "info" {
+//   domain            = improvmx_domain.dandiarchive.domain
+//   alias_name        = "info"
+//   destination_email = "emberarchive@jhuapl.edu"
+// }
+// 
+// resource "improvmx_email_forward" "team" {
+//   domain            = improvmx_domain.dandiarchive.domain
+//   alias_name        = "team"
+//   destination_email = "emberarchive@jhuapl.edu"
+// }
+// 
+// resource "improvmx_email_forward" "community" {
+//   domain            = improvmx_domain.dandiarchive.domain
+//   alias_name        = "community"
+//   destination_email = "emberarchive@jhuapl.edu"
+// }
