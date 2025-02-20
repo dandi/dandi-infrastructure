42bd8205e010c4317daf6ee994bf47ec3f282b02
Nicole Stock <nicole.stock@jhuapl.edu>
2025-02-20 16:09:34 -0500

    add 'data.' to improvmx domain references

--- terraform/email_forwarding.tf
+++ terraform/email_forwarding.tf
@@ -4,25 +4,25 @@ data "improvmx_domain" "dandiarchive" {
 
 # Set up email forwards.
 resource "improvmx_email_forward" "help" {
-  domain            = improvmx_domain.dandiarchive.domain
+  domain            = data.improvmx_domain.dandiarchive.domain
   alias_name        = "help"
   destination_email = "emberarchive@jhuapl.edu"
 }
 
 resource "improvmx_email_forward" "info" {
-  domain            = improvmx_domain.dandiarchive.domain
+  domain            = data.improvmx_domain.dandiarchive.domain
   alias_name        = "info"
   destination_email = "emberarchive@jhuapl.edu"
 }
 
 resource "improvmx_email_forward" "team" {
-  domain            = improvmx_domain.dandiarchive.domain
+  domain            = data.improvmx_domain.dandiarchive.domain
   alias_name        = "team"
   destination_email = "emberarchive@jhuapl.edu"
 }
 
 resource "improvmx_email_forward" "community" {
-  domain            = improvmx_domain.dandiarchive.domain
+  domain            = data.improvmx_domain.dandiarchive.domain
   alias_name        = "community"
   destination_email = "emberarchive@jhuapl.edu"
 }
