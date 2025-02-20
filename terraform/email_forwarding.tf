6da5a6965b2cd51c6b113107848e8f7edd414f99
Nicole Stock <nicole.stock@jhuapl.edu>
2025-02-20 16:20:19 -0500

    Reverted back to original. Commented out resource improvmx_domain. use string for improvmx_email_forward resources

--- terraform/email_forwarding.tf
+++ terraform/email_forwarding.tf
@@ -1,28 +1,28 @@
-data "improvmx_domain" "dandiarchive" {
+resource "improvmx_domain" "dandiarchive" {
   domain = "emberarchive.org"
 }
 
 # Set up email forwards.
 resource "improvmx_email_forward" "help" {
-  domain            = data.improvmx_domain.dandiarchive.domain
+  domain            = "emberarchive.org"
   alias_name        = "help"
   destination_email = "emberarchive@jhuapl.edu"
 }
 
 resource "improvmx_email_forward" "info" {
-  domain            = data.improvmx_domain.dandiarchive.domain
+  domain            = "emberarchive.org"
   alias_name        = "info"
   destination_email = "emberarchive@jhuapl.edu"
 }
 
 resource "improvmx_email_forward" "team" {
-  domain            = data.improvmx_domain.dandiarchive.domain
+  domain            = "emberarchive.org"
   alias_name        = "team"
   destination_email = "emberarchive@jhuapl.edu"
 }
 
 resource "improvmx_email_forward" "community" {
-  domain            = data.improvmx_domain.dandiarchive.domain
+  domain            = "emberarchive.org"
   alias_name        = "community"
   destination_email = "emberarchive@jhuapl.edu"
 }
