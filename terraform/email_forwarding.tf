1be6aac7b22b5a410d996d030b9d297b2e22438d
Lauren Diaz <lauren.diaz@jhuapl.edu>
2025-02-27 11:27:11 -0500

    add import statements for improvmx domain

--- terraform/email_forwarding.tf
+++ terraform/email_forwarding.tf
@@ -2,12 +2,17 @@ resource "improvmx_domain" "dandiarchive" {
   domain = "emberarchive.org"
 }
 
-# Set up email forwards.
-resource "improvmx_email_forward" "help" {
-  domain            = improvmx_domain.dandiarchive.domain
-  alias_name        = "help"
-  destination_email = "emberarchive@jhuapl.edu"
+import {
+  to = improvmx_domain.dandiarchive
+  id = "emberarchive.org"
 }
+
+# Set up email forwards.
+//resource "improvmx_email_forward" "help" {
+//  domain            = improvmx_domain.dandiarchive.domain
+//  alias_name        = "help"
+//  destination_email = "emberarchive@jhuapl.edu"
+//}
 
 resource "improvmx_email_forward" "info" {
   domain            = improvmx_domain.dandiarchive.domain
@@ -15,14 +20,20 @@ resource "improvmx_email_forward" "info" {
   destination_email = "emberarchive@jhuapl.edu"
 }
 
-resource "improvmx_email_forward" "team" {
-  domain            = improvmx_domain.dandiarchive.domain
-  alias_name        = "team"
-  destination_email = "emberarchive@jhuapl.edu"
+import {
+  to = improvmx_email_forward.info
+  id = "emberarchive.org_info"
 }
 
-resource "improvmx_email_forward" "community" {
-  domain            = improvmx_domain.dandiarchive.domain
-  alias_name        = "community"
-  destination_email = "emberarchive@jhuapl.edu"
-}
+
+//resource "improvmx_email_forward" "team" {
+//  domain            = improvmx_domain.dandiarchive.domain
+//  alias_name        = "team"
+//  destination_email = "emberarchive@jhuapl.edu"
+//}
+
+//resource "improvmx_email_forward" "community" {
+//  domain            = improvmx_domain.dandiarchive.domain
+//  alias_name        = "community"
+//  destination_email = "emberarchive@jhuapl.edu"
+//}
