41114d63c2ed28056100d45a67fbd5399d0756fb
Sandy Hider <sandy.hider@jhuapl.edu>
2025-03-03 14:33:25 -0500

    changes to go from staging to standbox

--- terraform/domain.tf
+++ terraform/domain.tf
@@ -20,10 +20,10 @@ resource "aws_route53_record" "gui" {
 
 resource "aws_route53_record" "gui-staging" {
   zone_id = aws_route53_zone.dandi.zone_id
-  name    = "gui-staging"
+  name    = "gui-sandbox"
   type    = "CNAME"
   ttl     = "300"
-  records = ["gui-dandi-staging-emberarchive-org.netlify.com"]
+  records = ["gui-dandi-sandbox-emberarchive-org.netlify.com"]
 }
 
 resource "aws_route53_record" "www" {
@@ -87,18 +87,3 @@ resource "aws_route53_record" "email-spf" {
 #   records = ["did=did:plc:5tjxaioq3ynbbynnarq5dziq"]
 # }
 
-// resource "aws_route53_record" "api-dandi-staging-heroku-app" {
-//   zone_id = aws_route53_zone.dandi.zone_id
-//   name    = "api-dandi-staging"
-//   type    = "CNAME"
-//  ttl     = "300"
-//  records = ["dry-waters-tfs5sbdos7ion2o6614l4dcm.herokudns.com"]
-// }
-// 
-// resource "aws_route53_record" "api-dandi-heroku-app" {
-//   zone_id = aws_route53_zone.dandi.zone_id
-//   name    = "api-dandi"
-//   type    = "CNAME"
-//   ttl     = "300"
-//   records = ["corrugated-gorilla-f2e8ls3ewsw7fv2ilslqkij0.herokudns.com"]
-// }
