7fe5c0c6a2f7b8ec1d27c87d637fbcb0473f7597
Jacob Nesbitt <jjnesbitt2@gmail.com>
2025-10-20 13:37:40 -0400

    Use "sandbox" instead of "staging"

--- terraform/staging_pipeline.tf
+++ terraform/staging_pipeline.tf
@@ -42,7 +42,7 @@ module "api_sandbox_heroku" {
     DJANGO_DANDI_DOI_API_USER         = "dartlib.dandi"
     DJANGO_DANDI_DOI_API_PREFIX       = "10.80507"
     DJANGO_DANDI_DOI_PUBLISH          = "false"
-    DJANGO_DANDI_INSTANCE_NAME        = "DANDI-STAGING"
+    DJANGO_DANDI_INSTANCE_NAME        = "DANDI-SANDBOX"
     DJANGO_DANDI_INSTANCE_IDENTIFIER  = "RRID:SCR_017571"
 
     # These may be removed in the future
